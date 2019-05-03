0880e68b35e7cdf7eab11659b1d19411e3082f74
Patrick Szalewicz <github@fachinformatiker.app>
2019-05-03 21:13:49 +0100

    add converting from Strings to Integers in the Arguments

--- src/app/fachinformatiker/myMashup/Main.java
+++ src/app/fachinformatiker/myMashup/Main.java
@@ -3,6 +3,20 @@ package app.fachinformatiker.myMashup;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int producer = 0;
+        int consumer = 0;
+        int sync = 0;
+
+        try {
+            producer = Integer.parseInt(args[0]);
+            consumer = Integer.parseInt(args[1]);
+            sync = Integer.parseInt(args[2]);
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("You have to pass me integers, not strings!");
+            System.exit(1);
+        }
+
+        System.out.println(producer + " " + consumer + " " + sync);
     }
 }
