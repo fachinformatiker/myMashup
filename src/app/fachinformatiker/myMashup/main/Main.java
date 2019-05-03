aac0d4d3e4eb6616a40dbbbe715a80521a53b9c1
Patrick Szalewicz <github@fachinformatiker.app>
2019-05-03 22:06:06 +0100

    fill CandyModel, CandyView and CandyController with functions, remove loadValues from model

--- src/app/fachinformatiker/myMashup/main/Main.java
+++ src/app/fachinformatiker/myMashup/main/Main.java
@@ -1,42 +1,21 @@
 package app.fachinformatiker.myMashup.main;
 
-//import app.fachinformatiker.myMashup.model.loadValues;
-
 import app.fachinformatiker.myMashup.control.CandyController;
 import app.fachinformatiker.myMashup.model.CandyModel;
 import app.fachinformatiker.myMashup.view.CandyView;
 
 public class Main {
 
-    //private static loadValues loadValues;
-
     public static void main(String[] args) {
-        //loadValues = new loadValues(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-        //System.out.println(loadValues);
 
-        CandyModel model = retrieveValues();
-
+        CandyModel model = new CandyModel();
         CandyView view = new CandyView();
-
         CandyController controller = new CandyController(model, view);
 
+        controller.setProducerValue(Integer.parseInt(args[0]));
+        controller.setConsumerValue(Integer.parseInt(args[1]));
+        controller.setSyncValue(Integer.parseInt(args[2]));
         controller.updateView();
-
-        controller.setProducerValue(10);
-
-        controller.setConsumerValue(5);
-
-        controller.setSyncValue(3);
-
-        controller.updateView();
-    }
-
-    private static CandyModel retrieveValues() {
-        CandyModel model = new CandyModel();
-        model.setProducer(1);
-        model.setConsumer(2);
-        model.setSync(3);
-        return model;
     }
 
 }
